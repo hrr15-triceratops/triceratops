be47272b762d74abffb149dead9d293d7cc2d81a
tmpace <tmpace11@gmail.com>
2016-04-07 12:13:42 -0700

    Add ui router & structure

---
+++ client/app/app.js
@@ -0,0 +1,17 @@
+angular.module('app', [
+  'ui.router'
+  ])
+
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/feed');
+
+    $stateProvider
+      .state('feed', {
+        url: '/feed',
+        templateUrl: 'app/components/feed/feedView.html'
+      })
+      .state('create', {
+        url: '/create',
+        templateUrl: 'app/components/create/createView.html'
+      })
+  });
