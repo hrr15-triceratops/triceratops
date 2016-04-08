aa70c630db57e437421d753a96d20f1820cb7ba4
Mark Keith <mark.r.keith@gmail.com>
2016-04-08 14:21:05 -0700

    (feat) Add working requestHandler for project create

--- server/utils/requestHandler.js
+++ server/utils/requestHandler.js
@@ -5,22 +5,23 @@ var Project = require('../db/projectSchema');
 var User = require('../db/userSchema');
 
 //Helper function that adds a new Project to the DB
-var create = function(req, res) {
+exports.create = function(req, res) {
   //this is a JSON object ready for sending to the DB
   var projectData = req.body;
   // Check if project title already exists in the DB
-  Project.find({ title: projectData.title, location: projectData.location }).then(function(found) {
-    if(found) {
+  Project.findOne({ title: projectData.title, location: projectData.location }, function(err, found) {
+    if(err) {
+      res.send(err);
+    } else if(found) {
       //send a response that the project title is already taken
       //TODO: Consider if we want to suggest that the project already exists... maybe send back the project we found
-      return res.status(403).send('Sorry, but the project name is already taken!');
-    } 
-    else {
+      res.send(found); //TODO: Maybe augment the found object to indicate that it's a duplicate
+    } else {
       // create a new project entry in the DB
       var project = new Project(projectData);
       // Save it in the DB
       project.save(function(err, project) {
-        if(err) return console.log(err);
+        if(err) res.send(err);
         //return the project to the client for rendering
         res.send(project);
       });
@@ -28,6 +29,7 @@ var create = function(req, res) {
   });
 };
 
-module.exports = {
-  create: create
-}
+exports.contrib = function(req, res) {};
+exports.rep = function(req, res) {};
+exports.projects = function(req, res) {};
+
