c1c5b094755775b289b451252cb6a7d6ef17215c
Mark Keith <mark.r.keith@gmail.com>
2016-04-11 14:03:57 -0700

    (fix) Update userSchema to have userFields and projectArray

--- server/db/userSchema.js
+++ server/db/userSchema.js
@@ -2,8 +2,11 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
-  name: { type: String, required: true },
-  location: { type: String, required: false }
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: false },
+  email: { type: String, required: true },
+  location: { type: String, required: false },
+  projects: { type: Array, required: false }
 }, { timestamps: true });
 
 var User = mongoose.model('User', userSchema);
