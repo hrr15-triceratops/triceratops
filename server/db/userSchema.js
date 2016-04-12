598b9a53e09e164d7e5c236736ddb371b3c49e4d
Mark Keith <mark.r.keith@gmail.com>
2016-04-12 15:24:46 -0700

    (feat) Add Authentication (#70)
    * (feat) Add authentication

--- server/db/userSchema.js
+++ server/db/userSchema.js
@@ -10,6 +10,13 @@ var userSchema = new Schema({
   projects: { type: Array, required: false }
 }, { timestamps: true });
 
+userSchema.methods.validPassword = function(password) {
+  if (this.password === password) {
+    return true;
+  }
+  return false;
+};
+
 var User = mongoose.model('User', userSchema);
 
 module.exports = User;
