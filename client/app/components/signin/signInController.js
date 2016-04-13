706cb5d981ecf1bca2dc0c069a3f96fdd1ab48ee
tmpace <tmpace11@gmail.com>
2016-04-12 17:51:10 -0700

    (feat) Adds basic authentication to the client * Routes are protected through the 'auth' property in the state manager * Users are redirected to /login if they try to access a route that requires auth

---
+++ client/app/components/signin/signInController.js
@@ -0,0 +1,19 @@
+angular.module('app.signIn', ['signIn.factory', 'ngCookies'])
+  .controller('signInController', ['signInFactory', '$cookies', function(signInFactory, $cookies) {
+
+    this.signIn = function() {
+      var data = {
+        email: this.email,
+        password: this.password
+      };
+
+      signInFactory
+        .login(data)
+        .then(function(user) {
+          if(!user) {
+            return console.log('Login failed');
+          }
+          $cookies.put('email', user.data.email);
+        });
+    };
+  }]);
