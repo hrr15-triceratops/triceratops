cb66aa4749e657924b6abfdcf66a4bb18f50a0cb
Trevor Pace <tmpace11@gmail.com>
2016-04-14 11:41:44 -0700

    Refactor/feed controller (#104)
    * (fix) Capitalized case-sensitive file requirement
    
    * (refactor) Refactor feedController to only display projects the user hasn't interacted with before
    
    * (fix) Fixed contributor message to only show if there are more than 3 contribs
    
    * (refactor) Refactored the algorithm to display only new projects to a user to be a linear complexity rather than quadratic

--- client/app/components/feed/feedController.js
+++ client/app/components/feed/feedController.js
@@ -2,17 +2,41 @@ angular.module('app.feed', ['feed.factory', 'ngCookies'])
   .controller('feedController', ['feedFactory', '$cookies', function(feedFactory, $cookies) {
     // Hold onto context
     var self = this;
+
+    // Grab variables from cookies
     var uid = $cookies.getAll().id;
     var name = $cookies.getAll().name;
 
-    // List of projects
+    // List of projects & our current user
     this.projects = [];
+    this.user = {};
+
+    // Object containing the ids of the users projects
+    var userProjects = {};
+
+    // Grab the current logged in user from the db so we can access their array of projects
+    feedFactory.getUser(uid)
+      .then(function(user) {
+        self.user = user.data;
+
+        // Build an object out of the user projects
+        self.user.projects.forEach(function(project) {
+          if (! (project.id in userProjects)) {
+            userProjects[project.id] = true;
+          }
+        });
+      });
+
+    // Make an object out of their contributions
+
 
     // Grab the projects from the server
     feedFactory.getProjects()
       .then(function(projects) {
-        self.projects = projects.data;
-        console.log(self.projects);
+        self.projects = projects.data.filter(function(project) {
+          // Only return the projects that the user hasn't contributed to
+          return (!(project._id in userProjects));
+        });
       });
 
     // function to handle positive rep
