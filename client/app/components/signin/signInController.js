8b5d508bc1a09780d5ed3588e50f3bed9f9f181f
tmpace <tmpace11@gmail.com>
2016-04-14 13:23:12 -0700

    (fix) Successful logins redirect to #/feed

--- client/app/components/signin/signInController.js
+++ client/app/components/signin/signInController.js
@@ -1,5 +1,5 @@
 angular.module('app.signIn', ['signIn.factory', 'ngCookies'])
-  .controller('signInController', ['signInFactory', '$cookies', function(signInFactory, $cookies) {
+  .controller('signInController', ['signInFactory', '$cookies', '$location', function(signInFactory, $cookies, $location) {
 
     this.signIn = function() {
       var data = {
@@ -13,9 +13,13 @@ angular.module('app.signIn', ['signIn.factory', 'ngCookies'])
           if(!user) {
             return console.log('Login failed');
           }
+          // Set our information in the cookie
           $cookies.put('name', user.data.firstName);
           $cookies.put('id', user.data._id);
           $cookies.put('email', user.data.email);
+
+          // Redirect user to the feed
+          $location.path('#/feed');
         });
     };
   }]);
